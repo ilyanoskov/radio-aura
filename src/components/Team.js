d9c6b7c14e474332da40440ea3b5fa09ee57a24e
ilyanoskov <riesig88@gmail.com>
2018-09-10 07:54:50 +0300

    making team component look better :)

--- src/components/Team.js
+++ src/components/Team.js
@@ -8,8 +8,8 @@ import { isEmpty } from 'lodash';
 const Container = styled('div')`
   display: flex;
   flex-direction: row;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
+  justify-content: flex-start;
   overflow-x: scroll;
 `;
 
@@ -22,6 +22,11 @@ const StyledDiv = styled('div')`
   }
 `;
 
+const SubSection = styled('div')`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
 const Team = ({ djs, sales, producers }) => {
   return (
     <StyledDiv>
@@ -30,43 +35,48 @@ const Team = ({ djs, sales, producers }) => {
           {' '}
           Team{' '}
         </Heading>
-        <Heading h="h2"> DJs </Heading>
-        <Container>
-          {!isEmpty(djs.items) &&
-            djs.items.map(dj => (
-              <TeamMember
-                name={dj.fields.name}
-                photo={dj.fields.photo.fields.file.url}
-                key={dj.sys.createdAt}
-              />
-            ))}
-          <TeamMember />
-        </Container>
-        <Heading h="h2"> SALES </Heading>
-        <Container>
-          {!isEmpty(sales.items) &&
-            sales.items.map(sale => (
-              <TeamMember
-                name={sale.fields.name}
-                photo={sale.fields.photo.fields.file.url}
-                key={sale.sys.createdAt}
-              />
-            ))}
-        </Container>
-        <Heading h="h2" style={{ margin: 0 }}>
-          {' '}
-          Producers{' '}
-        </Heading>
-        <Container>
-          {!isEmpty(producers.items) &&
-            producers.items.map(producer => (
-              <TeamMember
-                name={producer.fields.name}
-                photo={producer.fields.photo.fields.file.url}
-                key={producer.sys.createdAt}
-              />
-            ))}
-        </Container>
+        <SubSection>
+          <Heading h="h2"> DJs </Heading>
+          <Container>
+            {!isEmpty(djs.items) &&
+              djs.items.map(dj => (
+                <TeamMember
+                  name={dj.fields.name}
+                  photo={dj.fields.photo.fields.file.url}
+                  key={dj.sys.createdAt}
+                />
+              ))}
+          </Container>
+        </SubSection>
+        <SubSection>
+          <Heading h="h2"> SALES </Heading>
+          <Container>
+            {!isEmpty(sales.items) &&
+              sales.items.map(sale => (
+                <TeamMember
+                  name={sale.fields.name}
+                  photo={sale.fields.photo.fields.file.url}
+                  key={sale.sys.createdAt}
+                />
+              ))}
+          </Container>
+        </SubSection>
+        <SubSection>
+          <Heading h="h2" style={{ margin: 0 }}>
+            {' '}
+            Producers{' '}
+          </Heading>
+          <Container>
+            {!isEmpty(producers.items) &&
+              producers.items.map(producer => (
+                <TeamMember
+                  name={producer.fields.name}
+                  photo={producer.fields.photo.fields.file.url}
+                  key={producer.sys.createdAt}
+                />
+              ))}
+          </Container>
+        </SubSection>
       </Section>
     </StyledDiv>
   );
