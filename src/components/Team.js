f4aef622a7ace9d0965819e542e5cc0ff35d7306
ilyanoskov <riesig88@gmail.com>
2018-09-11 16:55:18 +0300

    trying custom scrollbars

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import styled from 'react-emotion';
+import Scrollbars from 'react-custom-scrollbars';
+import { isEmpty } from 'lodash';
 import Heading from './elements/Heading';
 import Section from './elements/Section';
 import TeamMember from './TeamMember';
-import { isEmpty } from 'lodash';
 
 const Container = styled('div')`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
   justify-content: flex-start;
-  overflow-x: scroll;
 `;
 
 const StyledDiv = styled('div')`
@@ -27,59 +27,78 @@ const SubSection = styled('div')`
   margin-bottom: 30px;
 `;
 
-const Team = ({ djs, sales, producers }) => {
+const Cont = ({ children }) => {
   return (
-    <StyledDiv>
-      <Section id="team">
-        <Heading h="h1" style={{ padding: '0' }}>
-          {' '}
-          Team{' '}
-        </Heading>
-        <SubSection>
-          <Heading h="h2"> DJs </Heading>
-          <Container>
-            {!isEmpty(djs.items) &&
-              djs.items.map(dj => (
-                <TeamMember
-                  name={dj.fields.name}
-                  photo={dj.fields.photo.fields.file.url}
-                  key={dj.sys.createdAt}
-                />
-              ))}
-          </Container>
-        </SubSection>
-        <SubSection>
-          <Heading h="h2"> SALES </Heading>
-          <Container>
-            {!isEmpty(sales.items) &&
-              sales.items.map(sale => (
-                <TeamMember
-                  name={sale.fields.name}
-                  photo={sale.fields.photo.fields.file.url}
-                  key={sale.sys.createdAt}
-                />
-              ))}
-          </Container>
-        </SubSection>
-        <SubSection>
-          <Heading h="h2" style={{ margin: 0 }}>
-            {' '}
-            Producers{' '}
-          </Heading>
-          <Container>
-            {!isEmpty(producers.items) &&
-              producers.items.map(producer => (
-                <TeamMember
-                  name={producer.fields.name}
-                  photo={producer.fields.photo.fields.file.url}
-                  key={producer.sys.createdAt}
-                />
-              ))}
-          </Container>
-        </SubSection>
-      </Section>
-    </StyledDiv>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        overflowX: 'scroll'
+      }}
+    >
+      {children}
+    </div>
   );
 };
+
+class Team extends React.Component {
+  render() {
+    const { djs, sales, producers } = this.props;
+    return (
+      <StyledDiv>
+        <Section id="team">
+          <Heading h="h1" style={{ padding: '0' }}>
+            {' '}
+            Team{' '}
+          </Heading>
+          <SubSection>
+            <Heading h="h2"> DJs </Heading>
+            <Container>
+              {!isEmpty(djs.items) &&
+                djs.items.map(dj => (
+                  <TeamMember
+                    name={dj.fields.name}
+                    photo={dj.fields.photo.fields.file.url}
+                    key={dj.sys.createdAt}
+                  />
+                ))}
+            </Container>
+          </SubSection>
+          <SubSection>
+            <Heading h="h2"> SALES </Heading>
+            <Scrollbars autoHide renderView={Cont}>
+              {!isEmpty(sales.items) &&
+                sales.items.map(sale => (
+                  <TeamMember
+                    name={sale.fields.name}
+                    photo={sale.fields.photo.fields.file.url}
+                    key={sale.sys.createdAt}
+                  />
+                ))}
+            </Scrollbars>
+          </SubSection>
+          <SubSection>
+            <Heading h="h2" style={{ margin: 0 }}>
+              {' '}
+              Producers{' '}
+            </Heading>
+            <Scrollbars autoHide renderView={Cont}>
+              {!isEmpty(producers.items) &&
+                producers.items.map(producer => (
+                  <TeamMember
+                    name={producer.fields.name}
+                    photo={producer.fields.photo.fields.file.url}
+                    key={producer.sys.createdAt}
+                  />
+                ))}
+            </Scrollbars>
+          </SubSection>
+        </Section>
+      </StyledDiv>
+    );
+  }
+}
 
 export default Team;
